a8703b2bd6c7d4e7a35d85c66f6e9ca44807feec
Shivam Kedia <65805659+shivamkedia17@users.noreply.github.com>
2022-09-23 11:58:20 +0530

    breaking forloop in py

---
+++ Python Code/forloop.py
@@ -0,0 +1,29 @@
+import numpy as np  
+n = 1
+for i in np.arange(0,0.0000000001,0.00000000001):
+    print(n,end =" ")
+    n+=1
+
+print("\n")
+n = 1 
+for i in np.arange(0,0.00000001,0.000000001):
+    print(n,end =" ")
+    n+=1
+
+print("\n")
+n = 1
+for i in np.arange(0,0.00001,0.000001):
+    print(n,end =" ")
+    n+=1
+
+print("\n")
+n = 1
+for i in np.arange(0,0.1,0.01):
+    print(n,end =" ")
+    n+=1
+
+print("\n")
+n = 1
+for i in np.arange(0,1,0.1):
+    print(n,end =" ")
+    n+=1
