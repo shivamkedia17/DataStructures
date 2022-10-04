7cadecc6f7651c45a8074c187d3152e1c6ac5cb8
Shivam Kedia <65805659+shivamkedia17@users.noreply.github.com>
2022-10-04 18:11:40 +0530

    made forloop worse using double,

--- C/forloop.c
+++ C/forloop.c
@@ -2,8 +2,15 @@
 
 int main(void)
 {
-    int i = 0;
-    for (float x = 0; x < 0.00000001; x=x+0.000000001)
+    int i = 1;
+    for (double x = 0; x < 0.0000000000000000000001; x=x+0.00000000000000000000001)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    printf("\n");
+    i = 1;
+    for (float x = 0; x < 10; x=x+1)
     {
         printf("%d\n",i);
         i++;
