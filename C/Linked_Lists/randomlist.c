a0852465373621c8daf3512b05d0fa42c37d352e
Shivam Kedia <65805659+shivamkedia17@users.noreply.github.com>
2022-10-08 22:49:15 +0530

    generates random list given size

---
+++ C/Linked_Lists/randomlist.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.c"
+#include "arrays.c"
+
+int main(int argc, char** argv)
+{
+    int l = 0;
+    switch (argc)
+    {
+        case 1:
+            printf("Enter Size of List: ");
+            scanf("%d",&l);  
+            break;
+
+        case 2:
+            sscanf(argv[1], "%d", &l);
+            break;
+
+        default:
+            fprintf(stderr, "Usage: %s [list_size]\n", argv[0]);
+            return 1; 
+            break;
+    }
+
+    int *A = genArray(l);
+    printArray(l, A);
+
+    nodePointer head = genlistFromArray(l, A); 
+
+    printf("Linked List: ");
+    printLinkedList(head);
+
+    free(A);
+    freeList(head);
+}
