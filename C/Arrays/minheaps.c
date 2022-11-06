afc2dce523e541416a91ddabcdd3507c8da18a6b
Shivam Kedia <65805659+shivamkedia17@users.noreply.github.com>
2022-11-06 16:50:15 +0530

    added extract_pop()

--- C/Arrays/minheaps.c
+++ C/Arrays/minheaps.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-#define LEN(A) (sizeof(A)/sizeof(A[0]))
+#include "heaps.c"
 
 int insert(int *H, int l,int val);
-void swap(int *H, int a, int b);
 void heapify_up(int *H, int child);
 void heapify_down(int *H, int l, int parent);
-bool isparent(int n, int l);
 int swapleast(int *H, int l, int p);
 void extract_min(int *H, int l);
-int findchild_left(int p);
-int findchild_right(int p);
-int findparent(int c);
-int min(int *H, int l, int a, int b);
+int extract_min_pop(int *H, int l);
 
 //returns new length of H
 int insert(int *H, int l, int val)
@@ -37,11 +31,18 @@ void extract_min(int *H, int l)
     heapify_down(H, l, 0);
 }
 
-void swap(int *H, int a, int b)
+//returns new length of H
+int extract_min_pop(int *H, int l)
 {
-    int temp = H[a];
-    H[a] = H[b];
-    H[b] = temp;
+    swap(H, 0, l-1);
+    heapify_down(H, l, 0);
+
+    int newlen = l - 1;
+    
+    //delete last element
+    H = realloc(H, newlen * sizeof(int));
+
+    return newlen;
 }
 
 void heapify_up(int *H, int child)
@@ -65,12 +66,6 @@ void heapify_down(int *H, int l, int parent)
     return;
 }
 
-bool isparent(int n, int l)
-{
-    if(findchild_right(n) >= l)     {return false;}
-    else                            {return true; }  
-}
-
 int swapleast(int *H, int l, int p)
 {
     //find left and right child, 
@@ -81,31 +76,4 @@ int swapleast(int *H, int l, int p)
     int least = min(H, l, p, min(H, l, left, right));
     if (p != least) {swap(H, p, least);}
     return least;
-}
-
-int findchild_left(int p)
-{
-    int c = (2*p) + 1;
-    return c;
-}
-
-int findchild_right(int p)
-{
-    int c = (2*p) + 2;
-    return c;
-}
-
-int findparent(int c)
-{
-    int p = (c-1) / 2;
-    return p;
-}
-
-//takes two indexes, returns the index containing the lesser value
-int min(int *H, int l, int a, int b)
-{
-    if (a >= l || b >= l) {return -1;}
-
-    int lesser_ind = H[a] < H[b] ? a:b;
-    return lesser_ind;
 }
