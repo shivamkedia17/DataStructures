2a186beebc0db32035ec7da5ad82e50987d643c0
Shivam Kedia <65805659+shivamkedia17@users.noreply.github.com>
2022-11-11 21:23:22 +0530

    reorganised repo 3

---
+++ C/Stable_Computation/abc.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int i;
+    int sum = 0;
+    for (i = 0; i -= 1000; i++)
+    {
+        sum = sum + i;
+        printf("%d\n",i);
+    }
+    printf("Hi\n");
+    return 0;
+}
